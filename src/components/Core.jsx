74e13e68d926e533e2d7dce963414e9e030bdfb5
Octopse <octopse.octopse@gmail.com>
2022-10-27 07:55:52 +0200

    Added the right side

--- src/components/Core.jsx
+++ src/components/Core.jsx
@@ -1,10 +1,14 @@
 import style from '../styles/core.module.css';
 import Hero from './Hero';
+import RightCore from './RightCore';
+import badge from '../assets/img/badge.svg';
 
 function Core(){
   return(
     <main className={style.core_container}>
       <Hero />
+      <RightCore />
+      <img src={badge} alt="The Badge" className={style.core_badge} />
     </main>
   );
 }
---
+++ src/components/RightCore.jsx
@@ -0,0 +1,42 @@
+import style from '../styles/rightcore.module.css';
+
+function RightCore(){
+  return(
+    <section className={style.rightcore}>
+      <div className={style.rightcore_container}>
+        <h2 className={style.rightcore_title}>We make your <span className={style.rightcore_title_nftinvestment}>NFT Investment</span> lot more easier</h2>
+        <p className={style.rightcore_detail}>We help you take your NFT Investments to next level with the help of our advanced blockchain infrastructure</p>
+        <button className={style.rightcore_button}>Explore collections</button>
+      {/* bottom part  */}
+      <div className={style.rightcore_startforyou}>
+        <h3 className={style.rightcore_startforyou_title}>Stats for you</h3>
+        <div className={style.rightcore_startforyou_items}>
+          <div>
+            <div className={style.rightcore_startforyou_items_container}>
+              <span className={style.rightcore_startforyou_items_value}>10K +</span>
+            </div>
+            <p className={style.rightcore_startforyou_items_text}>Auctions</p>
+          </div>
+          <div>
+            <div className={style.rightcore_startforyou_items_container}>
+              <span className={style.rightcore_startforyou_items_value}>200 +</span>
+            </div>
+            <p className={style.rightcore_startforyou_items_text}>Collections</p>
+          </div>
+          <div>
+            <div className={style.rightcore_startforyou_items_container}>
+              <span className={style.rightcore_startforyou_items_value}>50 +</span>
+            </div>
+            <p className={style.rightcore_startforyou_items_text}>Artists</p>
+          </div>
+        </div>
+        
+      </div>
+      </div>
+      
+    </section>
+    
+  );
+}
+
+export default RightCore;
